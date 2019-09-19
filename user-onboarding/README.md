8ad2cdfeee0384a07ba4203deead8ee068da5944
LenWinkler <lenwinkler86@gmail.com>
2019-09-19 18:41:50 -0400

    almost finished. receiving and displaying response from API

--- user-onboarding/README.md
+++ user-onboarding/README.md
@@ -34,7 +34,6 @@ Being able to `POST` data is a key skill of any developer, no matter your skill 
 
 - Craft a `POST` request using `axios` that sends your form data to the following endpoint: _https://reqres.in/api/users_
 - Verify using a `console.log()` that you are receiving a successful response back
-
 (Note: For those that are curious, we're using [reqres.in](https://reqres.in/) for this assignment's API. It's a free API that allows us to simulate a `POST` request for any data that we send it. Pretty awesome!)
 
 ## STEP 4 - Display Returned Data to Screen
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,33 +1,50 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-import { string } from "postcss-selector-parser";
 
 const UserForm = ({values, errors, touched, status}) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        if(status) {
+            setUsers([...users, status])
+        }
+    }, [status])
     return (
-        <Form>
-            <Field type="text" name="name" placeholder="Name" />
-            {touched.name && errors.name && (
-                <p>{errors.name}</p>
-            )}
+        <div className="user-form">
+            <Form className="actual-form">
+                <Field className="field" type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p>{errors.name}</p>
+                )}
 
-            <Field type="email" name="email" placeholder="Email" />
-            {touched.email && errors.email && (
-                <p>{errors.email}</p>
-            )}    
+                <Field className="field" type="email" name="email" placeholder="Email" />
+                {touched.email && errors.email && (
+                    <p>{errors.email}</p>
+                )}    
 
-            <Field type="password" name="password" placeholder="Password" />
-            {touched.password && errors.password && (
-                <p>{errors.password}</p>
-            )}    
+                <Field className="field" type="password" name="password" placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p>{errors.password}</p>
+                )}    
 
-            <label>Terms of Service
-                <Field type="checkbox" name="terms" />
-            </label>
-            <button type="submit">Submit</button>
-        </Form>
+                <label className="terms-text">
+                    <Field type="checkbox" name="terms" checked={values.terms}/>
+                    Terms of Service
+                </label>
+                <button>Submit</button>
+            </Form>
+            {users.map( user => (
+                <ul key={user.id}>
+                    <li>Name: {user.name}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Password: {user.password}</li>
+                </ul> 
+            ))}
+        </div>
+        
     )
+    
 }
 
 const FormikUserForm = withFormik({
@@ -42,7 +59,16 @@ const FormikUserForm = withFormik({
         name: Yup.string().required("Name is required").max(15, "Name must be 15 characters or less"),
         email: Yup.string().required("Email is required").email("Invalid email address"),
         password: Yup.string().required("Password is required").min(4, "Password must be between 4-8 characters").max(8, "Password must be between 4-8 characters")
-    })
+    }),
+
+    handleSubmit(values, { setStatus }) {
+        
+        axios.post('https://reqres.in/api/users', values)
+        .then(res => {
+            setStatus(res.data);
+        })
+        .catch(err => console.log('uh oh', err))
+    }
 })(UserForm);
 
 
